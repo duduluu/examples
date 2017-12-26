b23a117dcb581800321642bbc817f319097f4fb3
duduluu <duduluu@outlook.com>
2017-12-26 21:29:40 +0800

    Add example

---
+++ UWP_Example/Utils/Fruit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWP_Example.Utils {
+    public class Fruit {
+        static public string Apple { get { return "apple"; } }
+        static public string Banana { get { return "banana"; } }
+        static public string Orange { get { return "orange"; } }
+    }
+}
